5b6b4797586b7c315caffbe198a8e849e5197a94
3a632081 <s3a632081@student.ncut.edu.tw>
2019-12-11 18:57:05 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -25,7 +25,10 @@ class TaskController extends Controller
      */
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
     /**
      * 建立新的任務。
